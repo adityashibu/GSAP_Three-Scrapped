90858d4b987c0b9d07731c3bb86851f1cb9c2717
Aditya Shibu <as2397@hw.ac.uk>
2024-05-29 22:00:02 +0400

    Added components for main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="bg-black">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Highlights></Highlights>
+    </main>
   )
 }
 
