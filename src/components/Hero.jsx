ef69c66e9efb72900a3007ddae4a978721005d6d
Aditya Shibu <as2397@hw.ac.uk>
2024-06-01 11:04:14 +0400

    Removed highlights due to bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,22 +28,11 @@ const Hero = () => {
     }, [])
 
     return (
-        <section className="w-full nav-height bg-black relative">
-            <div className="h-5/6 w-full flex-center flex-col">
-                <p id="hero" className="hero-title">iPhone 15 Pro</p>
-                <div className="md:w-10/12 w-9/12">
-                    <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-                        <source src={videoSrc} type="video/mp4" />
-                    </video>
+        <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+            <div className="screen-max-width">
+                <div>
+                    <h1 id="title" className="section-heading">Get the highlights.</h1>
                 </div>
-            </div>
-
-            <div
-                id="cta"
-                className="flex flex-col items-center opacity-0 translate-y-20"
-            >
-                <a href="#highlights" className="btn">Buy</a>
-                <p className="font-normal text-xl">From AED 4,299</p>
             </div>
         </section>
     )
--- src/components/VideoCarousel.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VideoCarousel = () => {
-    return (
-        <div>VideoCarousel</div>
-    )
-}
-
-export default VideoCarousel
